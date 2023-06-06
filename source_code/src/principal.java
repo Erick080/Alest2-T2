77e54843a3dd53d793512fefd4dc61741d9304cf
Ovatmam <e.enes@edu.pucrs.br>
2023-06-06 18:07:46 -0300

    Now creates the graph and save the harbors' locations inside an array

--- source_code/src/principal.java
+++ source_code/src/principal.java
@@ -1,7 +1,8 @@
 package src;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
+import java.io.PrintWriter;
+//import java.util.HashMap;
 import java.util.Scanner;
 //import Casos_de_Teste.*;
 
@@ -9,33 +10,74 @@ public class principal {
   public static void main(String args[]) {
     long startTime, endTime;
     File arquivo;
-    int size, index, i;
-    String sizes[];
+    int size, index, i, harbors[];
+    String sizes[], data[], data2[];
     Scanner scanner;
+    Graph graph;
 
     try {
       startTime = System.currentTimeMillis(); //metodo para cronometrar tempo de execucao
       
-      arquivo = new File("Casos_De_Teste/case0.map");
+      arquivo = new File("./Casos_De_Teste/case0.map");
       scanner = new Scanner(arquivo);
 
-      // size[0] = altura, sizep[1] = largura
-      sizes = scanner.nextLine().split("");
+      // sizes[0] = altura, sizes[1] = largura
+      sizes = scanner.nextLine().split(" ");
       size = Integer.parseInt(sizes[0]);
       size *= Integer.parseInt(sizes[1]);
 
-      index = 0;
+      harbors = new int[9];
+
+      graph = new Graph(size);
+
+      index = -1;
+      data2 = scanner.nextLine().split("");
       while (scanner.hasNextLine()) { 
-        String[] data = scanner.nextLine().split("");
-        for(i=0; i < data.length; i++) {
-          //if(data[i].equals("."))
-            System.out.printf(data[i]);
+        data = data2;
+        data2 = scanner.nextLine().split("");
+
+        for(i=0; i < 50; i++) {
+          index++;
+          if(!data[i].equals("*")) {
+            if(!data[i].equals(".")) {
+              size = Integer.parseInt(data[i]);
+              harbors[size-1] = index;
+            }
+            if(i == 49) {
+              if(!data2[i].equals("*"))
+                graph.addEdge(index, 50+index);
+              continue;
+            }
+            if(!data2[i].equals("*")) {
+              graph.addEdge(index, 50+index);
+            }
+            if(!data[i+1].equals("*")) {
+              graph.addEdge(index, index+1);
+            }
+          }
         }
-        System.out.println("\n");
+      }
+
+      data = data2;
+      for(i=0; i < 50; i++) {
+        if(!data[i].equals("*")) {
+          if(!data[i].equals(".")) {
+            size = Integer.parseInt(data[i]);
+            harbors[size-1] = index+1;
+          }
+          if(i < 49 && !data[i+1].equals("*")) {
+            graph.addEdge(index, index+1);
+          }
+        }
         index++;
       }
       scanner.close();
-      
+      String resultado = graph.toDot();
+      PrintWriter out = new PrintWriter("dot.txt");
+      out.write(resultado);
+      out.close();
+      for(i=0;i<harbors.length;i++)
+        System.out.println(harbors[i]);
       endTime = System.currentTimeMillis();
       System.out.println("Tempo de execucao -  " + (endTime - startTime) + " millisegundos");
     }
@@ -43,7 +85,5 @@ public class principal {
       System.out.println("Aconteceu um erro");
       e.printStackTrace();
     }
-    
-    System.out.println("Hello World");
   }
 }
